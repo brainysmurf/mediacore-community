c28f214fe63b654d36cfd11fc3557ecd598074d6
Adam Morris <amorris@mistermorris.com>
2013-02-23 15:37:53 +0800

    trace_level defined in config

--- mediacore/config/environment.py
+++ mediacore/config/environment.py
@@ -102,6 +102,7 @@ def load_environment(global_conf, app_conf):
         'ouphrase': 'ou=folder',
         'dcphrase': 'dc=example,dc=org',
         'cnword': 'cn',
+        'trace_level': 0,  # set to 1 for better debugging with ldap
         'default_email_domain': 'example.org'  # users need an email address by default, what should it be?
     }
 
